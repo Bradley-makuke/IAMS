d69ddcaeb3659a1a407a0c90844445c05cff3cc0
Bradley-makuke <136990865+Bradley-makuke@users.noreply.github.com>
2024-05-06 14:25:20 +0200

    Update configure.php

--- configure.php
+++ configure.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "code");
+$conn = mysqli_connect("10.0.19.73", "tsu00073", "tsu00073", "db_tsu00073");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
